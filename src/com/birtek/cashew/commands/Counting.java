01e97d07564ab504af5c6e46790e6116128750ca
b1rtek <skrzeczek@onet.pl>
2022-03-08 13:05:09 +0100

    Counter can be reset now

--- src/com/birtek/cashew/commands/Counting.java
+++ src/com/birtek/cashew/commands/Counting.java
@@ -2,10 +2,12 @@ package com.birtek.cashew.commands;
 
 import com.birtek.cashew.Cashew;
 import com.birtek.cashew.Database;
+import com.birtek.cashew.messagereactions.CountingInfo;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import org.jetbrains.annotations.NotNull;
 
+import javax.swing.table.DefaultTableCellRenderer;
 import java.util.Locale;
 
 public class Counting extends BaseCommand {
@@ -20,8 +22,13 @@ public class Counting extends BaseCommand {
         if (args[0].equalsIgnoreCase(Cashew.COMMAND_PREFIX + "counting")) {
             if (checkPermissions(event, helpCommandPermissions)) {
                 String argument = parseInput(args);
-                if (saveToDatabase(argument.toLowerCase(Locale.ROOT), event.getChannel().getId())) {
-                    event.getMessage().reply("Counting has been turned " + args[1] + " in this channel.").mentionRepliedUser(false).queue();
+                if(argument.equalsIgnoreCase("reset")) {
+                    Database database = Database.getInstance();
+                    database.setCount(new CountingInfo(true, " ", 0), event.getChannel().getId());
+                } else {
+                    if (saveToDatabase(argument.toLowerCase(Locale.ROOT), event.getChannel().getId())) {
+                        event.getMessage().reply("Counting has been turned " + args[1] + " in this channel.").mentionRepliedUser(false).queue();
+                    }
                 }
             }
         }
