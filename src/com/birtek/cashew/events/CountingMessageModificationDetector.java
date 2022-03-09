53315622fe20c572379381e83331850479efbb56
B1rtek <b1rtek@fakemail.com>
2022-03-09 22:04:08 +0100

    Counter: anti-edit counting message patch

--- src/com/birtek/cashew/events/CountingMessageModificationDetector.java
+++ src/com/birtek/cashew/events/CountingMessageModificationDetector.java
@@ -2,20 +2,21 @@ package com.birtek.cashew.events;
 
 import com.birtek.cashew.Database;
 import com.birtek.cashew.messagereactions.CountingInfo;
-import net.dv8tion.jda.api.events.message.MessageDeleteEvent;
+import net.dv8tion.jda.api.events.message.MessageUpdateEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
 
-public class CountingMessageDeletionDetector extends ListenerAdapter {
+public class CountingMessageModificationDetector extends ListenerAdapter {
     @Override
-    public void onMessageDelete(MessageDeleteEvent event) {
+    public void onMessageUpdate(@NotNull MessageUpdateEvent event) {
         Database database = Database.getInstance();
         String channelID = event.getChannel().getId();
         CountingInfo info = database.getCountingData(channelID);
         if (info.getActive() && info.getMessageID().equals(event.getMessageId())) {
             String userName = "<@!" + info.getUserID() + ">";
-            Objects.requireNonNull(event.getJDA().getTextChannelById(channelID)).sendMessage(userName + " deleted their message with a count of ` " + info.getValue() + " `! The next number is ` "+(info.getValue()+1)+" `!").queue();
+            Objects.requireNonNull(event.getJDA().getTextChannelById(channelID)).sendMessage(userName + " edited their message with a count of ` " + info.getValue() + " `! The next number is ` "+(info.getValue()+1)+" `!").queue();
         }
     }
 }
