f42e132ff92ca4e2b4be8ca870062e17ab678747
b1rtek <skrzeczek@onet.pl>
2022-03-09 14:49:16 +0100

    Ping works now just like the JDA example

--- src/com/birtek/cashew/commands/Ping.java
+++ src/com/birtek/cashew/commands/Ping.java
@@ -11,21 +11,15 @@ public class Ping extends BaseCommand {
             Permission.MESSAGE_WRITE
     };
 
-    private long lastMeasured;
-
     @Override
     public void onGuildMessageReceived(@NotNull GuildMessageReceivedEvent event) {
         String pingMessage = "Pong!";
-        if(event.getMessage().getContentRaw().contentEquals(pingMessage) && event.getAuthor().getId().equals(Cashew.CASHEW_USER_ID)) {
-            long time = Math.round((System.nanoTime() - lastMeasured)/1000000.0);
-            event.getMessage().editMessage("Pong! Time = "+time+" ms").queue();
-            return;
-        }
         String[] args = event.getMessage().getContentRaw().split("\\s+");
         if (args[0].equalsIgnoreCase(Cashew.COMMAND_PREFIX + "ping")) {
-            if(checkPermissions(event, pingCommandPermissions)) {
-                event.getMessage().reply(pingMessage).mentionRepliedUser(false).queue();
-                lastMeasured = System.nanoTime();
+            if (checkPermissions(event, pingCommandPermissions)) {
+                long lastMeasured = System.nanoTime();
+                event.getMessage().reply(pingMessage).mentionRepliedUser(false).queue(response ->
+                        response.editMessage(pingMessage + " Time = " + Math.round((System.nanoTime() - lastMeasured) / 1000000.0) + " ms").queue());
             }
         }
     }
