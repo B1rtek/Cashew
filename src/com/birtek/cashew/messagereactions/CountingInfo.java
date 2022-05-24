6e8384eda1a1b02ae43b834c18ff57f1640ef3c9
b1rtek <skrzeczek@onet.pl>
2022-05-24 13:43:18 +0200

    CountingInfo: converted to record

--- src/com/birtek/cashew/messagereactions/CountingInfo.java
+++ src/com/birtek/cashew/messagereactions/CountingInfo.java
@@ -1,31 +1,4 @@
 package com.birtek.cashew.messagereactions;
 
-public class CountingInfo {
-    private final boolean active;
-    private final String userID;
-    private final int value;
-    private final String messageID;
-
-    public CountingInfo(boolean active, String userID, int value, String messageID) {
-        this.active = active;
-        this.userID = userID;
-        this.value = value;
-        this.messageID = messageID;
-    }
-
-    public boolean getActive() {
-        return active;
-    }
-
-    public String getUserID() {
-        return userID;
-    }
-
-    public int getValue() {
-        return value;
-    }
-
-    public String getMessageID() {
-        return messageID;
-    }
+public record CountingInfo(boolean active, String userID, int value, String messageID) {
 }
