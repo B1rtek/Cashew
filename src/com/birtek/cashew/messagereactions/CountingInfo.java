a95856c24b99c297413b4753cc14fa9d0ce585f6
b1rtek <skrzeczek@onet.pl>
2022-05-24 15:17:55 +0200

    CountingInfo: reverted from record because it completely broke everything

--- src/com/birtek/cashew/messagereactions/CountingInfo.java
+++ src/com/birtek/cashew/messagereactions/CountingInfo.java
@@ -1,4 +1,31 @@
 package com.birtek.cashew.messagereactions;
 
-public record CountingInfo(boolean active, String userID, int value, String messageID) {
+public class CountingInfo {
+    private final boolean active;
+    private final String userID;
+    private final int value;
+    private final String messageID;
+
+    public CountingInfo(boolean active, String userID, int value, String messageID) {
+        this.active = active;
+        this.userID = userID;
+        this.value = value;
+        this.messageID = messageID;
+    }
+
+    public boolean getActive() {
+        return active;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getMessageID() {
+        return messageID;
+    }
 }
