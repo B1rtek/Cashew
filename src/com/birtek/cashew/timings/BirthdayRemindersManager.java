a08cd14ff2f1bc646d80eeeb61e385ae9e5e4a3c
b1rtek <skrzeczek@onet.pl>
2022-06-06 09:18:25 +0200

    Removed accidental double scheduling

--- src/com/birtek/cashew/timings/BirthdayRemindersManager.java
+++ src/com/birtek/cashew/timings/BirthdayRemindersManager.java
@@ -39,7 +39,6 @@ public class BirthdayRemindersManager {
         }
         createDefaultsMap(defaultsList);
         BirthdayReminder.setJdaInstance(jdaInstance);
-        scheduleReminders();
     }
 
     /**
