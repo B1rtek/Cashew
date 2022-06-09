d10c24695d0d08f171cbbd2ad91d7767de1e464f
b1rtek <skrzeczek@onet.pl>
2022-06-09 11:26:47 +0200

    Counter: rewritten to allow the existence of auto count correction after being offline system :tm:

--- src/com/birtek/cashew/messagereactions/Counter.java
+++ src/com/birtek/cashew/messagereactions/Counter.java
@@ -123,6 +123,96 @@ public class Counter extends BaseReaction {
         return false; // no matches were found
     }
 
+    private String prepareMessage(String message) {
+        message = message.replace(',', '.');
+        // C++ patch lmao
+        if (message.length() > 2) {
+            if (message.startsWith("++")) {
+                message = "1+" + message.substring(2);
+            } else if (message.startsWith("--")) {
+                message = "-1+" + message.substring(2);
+            }
+            String substring = message.substring(0, message.length() - 2);
+            if (message.endsWith("++")) {
+                message = substring + "+1";
+            } else if (message.endsWith("--")) {
+                message = substring + "-1";
+            }
+        }
+        return message;
+    }
+
+    private enum MessageAnalysisResultType {
+        CORRECT, INCORRECT, TYPO, ERROR, DIV0
+    }
+
+    private record MessageAnalysisResult(MessageAnalysisResultType type, double result) {
+    }
+
+    private MessageAnalysisResult analyzeMessage(String message, int current) {
+        ExpressionBuilder test = new ExpressionBuilder(message);
+        double result;
+        try {
+            result = test.build().evaluate();
+        } catch (IllegalArgumentException | EmptyStackException e) {
+            return new MessageAnalysisResult(MessageAnalysisResultType.ERROR, 0);
+        } catch (ArithmeticException e) {
+            return new MessageAnalysisResult(MessageAnalysisResultType.DIV0, 0);
+        }
+        if (result == 0) {
+            return new MessageAnalysisResult(MessageAnalysisResultType.ERROR, 0);
+        }
+        if (result != current + 1) {
+            if (checkIfTypo(message, current + 1)) {
+                return new MessageAnalysisResult(MessageAnalysisResultType.TYPO, current);
+            } else {
+                return new MessageAnalysisResult(MessageAnalysisResultType.INCORRECT, result);
+            }
+        } else {
+            return new MessageAnalysisResult(MessageAnalysisResultType.CORRECT, current + 1);
+        }
+    }
+
+    private boolean handleTypo(MessageReceivedEvent event, double previous, int typosLeft) {
+        if(typosLeft <= 0) return false;
+        int newTyposLeft = typosLeft - 1;
+        event.getMessage().addReaction("⚠️").queue();
+        String plural = newTyposLeft == 1 ? "" : "s";
+        event.getChannel().sendMessage("<@!" + event.getAuthor().getId() + "> made a typo, but the count was saved. Count has been corrected to ` " + (int) (previous+1) + " `, **" + newTyposLeft + "** typo" + plural + " left! The next number is ` " + (int) (previous + 2) + " `!").queue();
+        Database database = Database.getInstance();
+        database.setCount(new CountingInfo(true, event.getAuthor().getId(), (int) (previous+1), event.getMessageId(), newTyposLeft), event.getChannel().getId());
+        return true;
+    }
+
+    private void handleCorrectCount(MessageReceivedEvent event, double result, int typosLeft) {
+        String reactionEmote = "✅";
+        if (result == 69) {
+            reactionEmote = "♋";
+        } else if (result == 100) {
+            reactionEmote = "\uD83D\uDCAF";
+        } else if (result == 420 || result == 2137) {
+            reactionEmote = "\uD83D\uDE01";
+        } else if (result == 1337) {
+            reactionEmote = "\uD83D\uDE0E";
+        }
+        event.getMessage().addReaction(reactionEmote).queue();
+        Database database = Database.getInstance();
+        database.setCount(new CountingInfo(true, event.getAuthor().getId(), (int) result, event.getMessageId(), typosLeft), event.getChannel().getId());
+
+    }
+
+    private void handleIncorrectCount(MessageReceivedEvent event, double result, int current) {
+        double rounded = Math.round(result);
+        String toOutput = String.valueOf(result);
+        if (rounded == result) {
+            toOutput = String.valueOf((int) result);
+        }
+        event.getMessage().addReaction("❌").queue();
+        event.getChannel().sendMessage("<@!" + event.getAuthor().getId() + "> screwed up by writing ` " + toOutput + " `! The next number should have been ` " + (current + 1) + " `! Counter has been reset!").queue();
+        Database database = Database.getInstance();
+        database.setCount(new CountingInfo(true, " ", 0, " ", 3), event.getChannel().getId());
+    }
+
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
         if (!checkIfNotBot(event) || event.isWebhookMessage()) {
@@ -135,66 +225,18 @@ public class Counter extends BaseReaction {
         Database database = Database.getInstance();
         CountingInfo countingData = database.getCountingData(event.getChannel().getId());
         if (countingData.active() && !Objects.equals(countingData.userID(), event.getAuthor().getId())) {
-            message = message.replace(',', '.');
-            // C++ patch lmao
-            if (message.length() > 2) {
-                if (message.startsWith("++")) {
-                    message = "1+" + message.substring(2);
-                } else if (message.startsWith("--")) {
-                    message = "-1+" + message.substring(2);
+            message = prepareMessage(message);
+            MessageAnalysisResult analysisResult = analyzeMessage(message, countingData.value());
+            switch (analysisResult.type()) {
+                case ERROR -> {
+                    // do absolutely nothing
                 }
-                String substring = message.substring(0, message.length() - 2);
-                if (message.endsWith("++")) {
-                    message = substring + "+1";
-                } else if (message.endsWith("--")) {
-                    message = substring + "-1";
+                case DIV0 -> event.getMessage().reply("This is illegal bruh").mentionRepliedUser(false).queue();
+                case CORRECT -> handleCorrectCount(event, analysisResult.result(), countingData.typosLeft());
+                case INCORRECT -> handleIncorrectCount(event, analysisResult.result(), countingData.value());
+                case TYPO -> {
+                    if (!handleTypo(event, analysisResult.result(),countingData.typosLeft())) handleIncorrectCount(event, analysisResult.result(), countingData.value());
                 }
-            }
-            ExpressionBuilder test = new ExpressionBuilder(message);
-            double result, current = countingData.value();
-            try {
-                result = test.build().evaluate();
-            } catch (IllegalArgumentException | EmptyStackException e) {
-                return;
-            } catch (ArithmeticException e) {
-                event.getMessage().reply("This is illegal bruh").mentionRepliedUser(false).queue();
-                return;
-            }
-            if (result == 0) {
-                return;
-            }
-            if (result != current + 1) {
-                int typosLeft = countingData.typosLeft();
-                if (checkIfTypo(message, current + 1) && typosLeft > 0) {
-                    int newTyposLeft = countingData.typosLeft() - 1;
-                    event.getMessage().addReaction("⚠️").queue();
-                    String plural = newTyposLeft == 1 ? "" : "s";
-                    event.getChannel().sendMessage("<@!" + event.getAuthor().getId() + "> made a typo, but the count was saved. Count has been corrected to ` " + (int) (current + 1) + " `, **" + newTyposLeft + "** typo" + plural + " left! The next number is ` " + (int) (current + 2) + " `!").queue();
-                    database.setCount(new CountingInfo(true, event.getAuthor().getId(), (int) current + 1, event.getMessageId(), newTyposLeft), event.getChannel().getId());
-                } else {
-                    double rounded = Math.round(result);
-                    String toOutput = String.valueOf(result);
-                    if (rounded == result) {
-                        toOutput = String.valueOf((int) result);
-                    }
-                    event.getMessage().addReaction("❌").queue();
-                    event.getChannel().sendMessage("<@!" + event.getAuthor().getId() + "> screwed up by writing ` " + toOutput + " `! The next number should have been ` " + (int) (current + 1) + " `! Counter has been reset!").queue();
-                    database.setCount(new CountingInfo(true, " ", 0, " ", 3), event.getChannel().getId());
-
-                }
-            } else {
-                String reactionEmote = "✅";
-                if (result == 69) {
-                    reactionEmote = "♋";
-                } else if (result == 100) {
-                    reactionEmote = "\uD83D\uDCAF";
-                } else if (result == 420 || result == 2137) {
-                    reactionEmote = "\uD83D\uDE01";
-                } else if (result == 1337) {
-                    reactionEmote = "\uD83D\uDE0E";
-                }
-                event.getMessage().addReaction(reactionEmote).queue();
-                database.setCount(new CountingInfo(true, event.getAuthor().getId(), (int) result, event.getMessageId(), countingData.typosLeft()), event.getChannel().getId());
             }
         }
     }
