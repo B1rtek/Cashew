ec8424c0f2fe99eecffc19f7b9ca30121b817603
B1rtek <b1rtek@fakemail.com>
2022-06-25 21:42:48 +0200

    Created a method that generates a png from the leaderboard table

--- src/com/birtek/cashew/commands/BaseCommand.java
+++ src/com/birtek/cashew/commands/BaseCommand.java
@@ -7,17 +7,23 @@ import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URLConnection;
-import java.util.ArrayList;
-import java.util.EnumSet;
-import java.util.Locale;
-import java.util.Objects;
+import java.util.*;
 
 public class BaseCommand extends ListenerAdapter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(BaseCommand.class);
 
     public Permission[] adminPermissions = {
             Permission.ADMINISTRATOR
@@ -80,7 +86,7 @@ public class BaseCommand extends ListenerAdapter {
         if (strNum == null) {
             return false;
         }
-        if(strNum.isEmpty()) {
+        if (strNum.isEmpty()) {
             return true;
         }
         int start = 0;
@@ -107,11 +113,11 @@ public class BaseCommand extends ListenerAdapter {
     public static ArrayList<String> autocompleteFromList(ArrayList<String> options, String typed) {
         ArrayList<String> matching = new ArrayList<>();
         for (String option : options) {
-            if(option.toLowerCase().contains(typed.toLowerCase(Locale.ROOT))) {
+            if (option.toLowerCase().contains(typed.toLowerCase(Locale.ROOT))) {
                 matching.add(option);
             }
         }
-        if(matching.size() > 25) {
+        if (matching.size() > 25) {
             matching = new ArrayList<>() {
                 {
                     add("There's more than 25 matching options");
@@ -123,5 +129,28 @@ public class BaseCommand extends ListenerAdapter {
 
     protected boolean isInvalidTimestamp(String timestring) {
         return timestring.length() != 8 || Integer.parseInt(timestring.substring(0, 2)) > 23 || Integer.parseInt(timestring.substring(3, 5)) > 59 || Integer.parseInt(timestring.substring(6, 8)) > 59;
+    }
+
+    protected String generateLeaderboard(ArrayList<LeaderboardRecord> leaderboardRecords, String pointsName) {
+        String[][] tableData = new String[leaderboardRecords.size()][3];
+        JTable table = new JTable(tableData, new String[]{"#", "User", pointsName});
+        // thanks https://coderanch.com/t/338608/java/save-jtable-image ^^
+        int width = Math.max(table.getTableHeader().getWidth(), table.getWidth());
+        int height = table.getHeight() + table.getTableHeader().getHeight();
+        BufferedImage bi = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g2 = bi.createGraphics();
+        table.getTableHeader().paint(g2);
+        g2.translate(0, table.getTableHeader().getHeight());
+        table.paint(g2);
+        g2.dispose();
+        Random random = new Random();
+        String fileName = "generated/leaderboardTable" + random.nextInt(10000) + ".png";
+        try {
+            ImageIO.write(bi, "png", new File(fileName));
+            return fileName;
+        } catch (IOException ioe) {
+            LOGGER.error("Failed to generate leaderboard " + pointsName);
+            return null;
+        }
     }
 }
