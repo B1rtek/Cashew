ef0473f86fcd1b8943457f0d2302c7813098470a
B1rtek <b1rtek@fakemail.com>
2022-07-19 21:25:58 +0200

    Removed the old GiftsDatabase code from the Database class

--- src/com/birtek/cashew/Database.java
+++ src/com/birtek/cashew/Database.java
@@ -3,7 +3,6 @@ package com.birtek.cashew;
 import com.birtek.cashew.commands.*;
 import com.birtek.cashew.timings.*;
 
-import java.awt.*;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -247,24 +246,6 @@ public final class Database {
             e.printStackTrace();
             System.err.println("An error occured while querying the Messages table.");
             return null;
-        }
-    }
-
-    public ArrayList<GiftInfo> getAvailableGifts() {
-        try {
-            PreparedStatement prepStmt = giftsConnection.prepareStatement("SELECT giftID, giftName, giftImageURL, reactionLine1, reactionLine2, displayName, color FROM Gifts");
-            ResultSet results = prepStmt.executeQuery();
-            ArrayList<GiftInfo> availableGifts = new ArrayList<>();
-            if (results == null) {
-                return availableGifts;
-            }
-            while (results.next()) {
-                availableGifts.add(new GiftInfo(results.getInt(1), results.getString(2), results.getString(3), results.getString(4), results.getString(5), results.getString(6), new Color(results.getInt(7))));
-            }
-            return availableGifts;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return new ArrayList<>();
         }
     }
 
