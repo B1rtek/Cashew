f1069b505109db60cc3ecbebc974ab7e2cb02846
B1rtek <skrzeczek@onet.pl>
2022-07-26 20:53:10 +0200

    Updated the LeaderboardRecord count field from int to long

--- src/com/birtek/cashew/database/LeaderboardRecord.java
+++ src/com/birtek/cashew/database/LeaderboardRecord.java
@@ -1,3 +1,3 @@
 package com.birtek.cashew.database;
 
-public record LeaderboardRecord(int place, String userID, int count) {}
+public record LeaderboardRecord(int place, String userID, long count) {}
