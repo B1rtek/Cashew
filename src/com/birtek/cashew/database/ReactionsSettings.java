1d0d76206accae1d954518063c0f1f49cb09a14a
B1rtek <skrzeczek@onet.pl>
2022-07-30 21:37:23 +0200

    Implemented the ReactionsSettings object which modifies the settings JSON and gets data from it

--- src/com/birtek/cashew/database/ReactionsSettings.java
+++ src/com/birtek/cashew/database/ReactionsSettings.java
@@ -2,25 +2,76 @@ package com.birtek.cashew.database;
 
 import org.json.JSONObject;
 
+import java.util.Iterator;
+
 /**
  * Class that stores message reactions settings
  */
 public class ReactionsSettings {
 
     private final String serverID;
-    private JSONObject settings;
+    private final JSONObject settings;
 
     ReactionsSettings(JSONObject jsonSettings, String serverID) {
         this.settings = jsonSettings;
         this.serverID = serverID;
     }
 
-    public boolean setActivity(int reactionID, String channelID) {
-        return false;
+    /**
+     * Sets the activity of a reaction to a certain state in the specified channel
+     *
+     * @param reactionID ID of the reaction to change settings of, 0 for all
+     * @param channelID  ID of the channel to change the setting in, set to "all" to set it in all channels
+     * @param state      new settings state - true is on, false is off
+     */
+    public void setActivity(int reactionID, String channelID, boolean state) {
+        String srID = String.valueOf(reactionID);
+        if(reactionID == 0) { // setting all - remove all settings and replace them with { "all": <state> }
+            Iterator<String> reactionsKeys = settings.keys();
+            while(reactionsKeys.hasNext()) {
+                String key = reactionsKeys.next();
+                settings.remove(key);
+            }
+            settings.put("all", state);
+        } else {
+            JSONObject reactionObject;
+            if(!settings.has(srID)) {
+                reactionObject = new JSONObject();
+            } else {
+                reactionObject = settings.getJSONObject(srID);
+            }
+            if(channelID.equals("all")) { // setting for all channels - remove settings for single channels
+                Iterator<String> channelsKeys = reactionObject.keys();
+                while(channelsKeys.hasNext()) {
+                    String key = channelsKeys.next();
+                    reactionObject.remove(key);
+                }
+            }
+            reactionObject.put(channelID, state);
+            settings.put(srID, reactionObject);
+        }
     }
 
-    public boolean getActivity(int reaction) {
-        return false;
+    public boolean getActivity(int reactionID, String channelID) {
+        boolean result = false;
+        // first check for a global setting
+        if(settings.has("all")) {
+            result = settings.getBoolean("all");
+        }
+        // next check for a reaction specific one
+        String srID = String.valueOf(reactionID);
+        if(settings.has(srID)) {
+            JSONObject reactionObject = settings.getJSONObject(srID);
+            // check for all channels setting
+            if(reactionObject.has("all")) {
+                result = reactionObject.getBoolean("all");
+            }
+            // next check for a channel specific one
+            if(reactionObject.has(channelID)) {
+                result = reactionObject.getBoolean(channelID);
+            }
+        }
+        return result;
     }
 
     public JSONObject getSettings() {
