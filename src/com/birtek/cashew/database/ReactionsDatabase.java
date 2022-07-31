c57d9da1eb8ef24a8edaab5d011f6387b585c617
B1rtek <skrzeczek@onet.pl>
2022-07-31 02:25:01 +0200

    Created the ReactionsDatabase which gets all reactions data for the bot

--- src/com/birtek/cashew/database/ReactionsDatabase.java
+++ src/com/birtek/cashew/database/ReactionsDatabase.java
@@ -1,14 +1,13 @@
 package com.birtek.cashew.database;
 
-import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.*;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class ReactionsDatabase {
-
     private static final Logger LOGGER = LoggerFactory.getLogger(ReactionsDatabase.class);
 
     private static volatile ReactionsDatabase instance;
@@ -16,36 +15,28 @@ public class ReactionsDatabase {
     private Connection reactionsConnection;
 
     /**
-     * Initializes the connection to the Postgres database, specifically to the
-     * reactions table
+     * Initializes the connection to the database located at
+     * databases/data/reactions.db
      * If the connection fails to be established or the JDBC driver isn't found,
      * the bot exits with status 1 as it can't properly function without the database
      */
     private ReactionsDatabase() {
         try {
-            Class.forName("org.postgresql.Driver");
+            Class.forName("org.sqlite.JDBC");
         } catch (ClassNotFoundException e) {
-            LOGGER.error("Missing Postgres JDBC driver");
+            LOGGER.error("Missing SQLite JDBC driver");
             e.printStackTrace();
             System.exit(1);
         }
 
         try {
-            reactionsConnection = DriverManager.getConnection(System.getenv("JDBC_DATABASE_URL"));
+            reactionsConnection = DriverManager.getConnection("jdbc:sqlite:databases/data/reactions.db");
         } catch (SQLException e) {
-            LOGGER.error("Couldn't connect to the Postgres database - database could be offline or the url might be wrong or being currently refreshed");
+            LOGGER.error("Couldn't connect to reactions.db - possibly missing database");
             e.printStackTrace();
             System.exit(1);
         }
 
-        try {
-            PreparedStatement preparedStatement = reactionsConnection.prepareStatement("CREATE TABLE IF NOT EXISTS reactions(serverid TEXT, settings TEXT)");
-            preparedStatement.execute();
-        } catch (SQLException e) {
-            LOGGER.error("Failed to create the reactions table");
-            e.printStackTrace();
-            System.exit(1);
-        }
     }
 
     /**
@@ -65,93 +56,34 @@ public class ReactionsDatabase {
     }
 
     /**
-     * Gets all reactions settings from the database and returns them in a HashMap mapping the server ID to the settings
+     * Retrieves all reactions from the database
      *
-     * @return HashMap with all settings from the database or null if an error occurred
+     * @return ArrayList of {@link Reaction Reactions} for every record in the database or null if an error occurred
      */
-    public HashMap<String, ReactionsSettings> getAllReactionsSettings() {
+    public ArrayList<Reaction> getAllReactions() {
         try {
-            PreparedStatement preparedStatement = reactionsConnection.prepareStatement("SELECT * FROM reactions");
+            PreparedStatement preparedStatement = reactionsConnection.prepareStatement("SELECT _id,  name, description, pattern, actionID, actionContent FROM reactions");
             ResultSet results = preparedStatement.executeQuery();
-            HashMap<String, ReactionsSettings> settingsHashMap = new HashMap<>();
+            ArrayList<Reaction> reactions = new ArrayList<>();
             while (results.next()) {
-                settingsHashMap.put(results.getString(1), new ReactionsSettings(new JSONObject(results.getString(2)), results.getString(1)));
+                reactions.add(new Reaction(results.getInt(1), results.getString(2), results.getString(3), createPatternsArrayList(results.getString(4)), results.getInt(5), results.getString(6)));
             }
-            return settingsHashMap;
+            return reactions;
         } catch (SQLException e) {
-            LOGGER.warn(e + " thrown at ReactionsDatabase.getAllReactionsSettings()");
+            LOGGER.warn(e + " thrown at ReactionsDatabase.getAllReactions()");
             return null;
         }
     }
 
     /**
-     * Sets new reactions settings for a server
+     * Splist the string by the commas and returns the split contents as an ArrayList
      *
-     * @param settings {@link ReactionsSettings ReactionSettings} object with settings of the server
-     * @return true if the setting was successful, false otherwise
+     * @param patterns String patterns separated by commas
+     * @return an ArrayList of Strings generated from the comma separated input String
      */
-    public boolean setReactionsSettings(ReactionsSettings settings) {
-        if (isInDatabase(settings.getServerID())) {
-            return updateReactionsSettings(settings);
-        } else {
-            return insertReactionsSettings(settings);
-        }
-    }
-
-    /**
-     * Checks whether a server has a record in the database
-     *
-     * @param serverID ID of the server to check for existence in the database
-     * @return true if the server has a record in the database, false if it doesn't or if an error occurred
-     */
-    private boolean isInDatabase(String serverID) {
-        try {
-            PreparedStatement preparedStatement = reactionsConnection.prepareStatement("SELECT COUNT(*) FROM reactions WHERE serverid = ?");
-            preparedStatement.setString(1, serverID);
-            ResultSet results = preparedStatement.executeQuery();
-            if (results.next()) {
-                return results.getInt(1) == 1;
-            }
-            return false;
-        } catch (SQLException e) {
-            LOGGER.warn(e + " thrown at ReactionsDatabase.isInDatabase()");
-            return false;
-        }
-    }
-
-    /**
-     * Updates server's reactions settings record in the database
-     *
-     * @param settings {@link ReactionsSettings ReactionsSettings} to update the record to
-     * @return true if the update was successful, false otherwise or if an error occurred
-     */
-    private boolean updateReactionsSettings(ReactionsSettings settings) {
-        try {
-            PreparedStatement preparedStatement = reactionsConnection.prepareStatement("UPDATE reactions SET settings = ? WHERE serverid = ?");
-            preparedStatement.setString(1, settings.getSettings().toString());
-            preparedStatement.setString(2, settings.getServerID());
-            return preparedStatement.executeUpdate() == 1;
-        } catch (SQLException e) {
-            LOGGER.warn(e + " thrown at ReactionsDatabase.updateReactionsSettings()");
-            return false;
-        }
-    }
-
-    /**
-     * Inserts server's reactions settings into the database
-     *
-     * @param settings {@link ReactionsSettings ReactionsSettings} to insert into the database
-     * @return true if the insertion was successful, false otherwise or if an error occurred
-     */
-    private boolean insertReactionsSettings(ReactionsSettings settings) {
-        try {
-            PreparedStatement preparedStatement = reactionsConnection.prepareStatement("INSERT INTO reactions(serverid, settings) VALUES(?, ?)");
-            preparedStatement.setString(1, settings.getServerID());
-            preparedStatement.setString(2, settings.getSettings().toString());
-            return preparedStatement.executeUpdate() == 1;
-        } catch (SQLException e) {
-            LOGGER.warn(e + " thrown at ReactionsDatabase.insertReactionsSettings()");
-            return false;
-        }
+    private ArrayList<String> createPatternsArrayList(String patterns) {
+        ArrayList<String> patternsArrayList = new ArrayList<>();
+        Collections.addAll(patternsArrayList, patterns.split(","));
+        return patternsArrayList;
     }
 }
