e74c93e30a0abbc121dc55a76864a5c24bab09fe
B1rtek <skrzeczek@onet.pl>
2022-08-01 18:43:52 +0200

    Added a way to roll up to 10000 times

--- src/com/birtek/cashew/commands/Roll.java
+++ src/com/birtek/cashew/commands/Roll.java
@@ -5,6 +5,7 @@ import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEve
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.ByteArrayInputStream;
 import java.util.Random;
 
 public class Roll extends BaseCommand {
@@ -15,7 +16,7 @@ public class Roll extends BaseCommand {
     public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
         if (event.getName().equals("roll")) {
             int sides = event.getOption("sides", 6, OptionMapping::getAsInt);
-            int rolls = Math.min(event.getOption("rolls", 1, OptionMapping::getAsInt), 100);
+            int rolls = Math.min(event.getOption("rolls", 1, OptionMapping::getAsInt), 10000);
             long sum = 0;
             StringBuilder stringResults = new StringBuilder();
             for (int i = 0; i < rolls; i++) {
@@ -25,12 +26,21 @@ public class Roll extends BaseCommand {
             }
             stringResults.delete(stringResults.length() - 2, stringResults.length());
             double mean = Math.round((double) sum / rolls * 100.0) / 100.0;
-            EmbedBuilder rollEmbed = new EmbedBuilder();
-            rollEmbed.setTitle("Roll");
-            rollEmbed.setDescription(sides + " sides, " + rolls + " rolls");
-            rollEmbed.addField("Results", stringResults.toString(), false);
-            rollEmbed.setFooter("Sum: " + sum, "mean: " + mean);
-            event.replyEmbeds(rollEmbed.build()).queue();
+            if (stringResults.length() > 1024) {
+                stringResults = new StringBuilder(stringResults.toString().replace(", ", "\n"));
+                stringResults.append("\n Sum: ").append(sum).append(", mean: ").append(mean);
+                event.reply("Results were too long to fit in an embed, so here's a file with all of them").queue();
+                event.getChannel().sendFile(new ByteArrayInputStream(stringResults.toString().getBytes()), "results.txt").queue();
+            } else {
+                EmbedBuilder rollEmbed = new EmbedBuilder();
+                rollEmbed.setTitle("Roll");
+                rollEmbed.setDescription(sides + " sides, " + rolls + " rolls");
+                rollEmbed.addField("Results", stringResults.toString(), false);
+                rollEmbed.setFooter("Sum: " + sum + ", mean: " + mean);
+                rollEmbed.setThumbnail("https://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/2-Dice-Icon.svg/1200px-2-Dice-Icon.svg.png");
+                event.replyEmbeds(rollEmbed.build()).queue();
+            }
+
         }
     }
 }
