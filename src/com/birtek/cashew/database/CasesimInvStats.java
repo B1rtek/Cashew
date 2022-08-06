79abca32393cc828134c0af50d2cb4a244dc7c36
B1rtek <skrzeczek@onet.pl>
2022-08-06 23:38:02 +0200

    Created a CasesimInvStats record which contains statistics about a user's Casesim inventory

---
+++ src/com/birtek/cashew/database/CasesimInvStats.java
@@ -0,0 +1,5 @@
+package com.birtek.cashew.database;
+
+public record CasesimInvStats(int casesOpened, int collectionsOpened, int capsulesOpened, int itemsInInventory,
+                              boolean isPublic) {
+}
