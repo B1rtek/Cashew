9873afc20568cbdbb44fd97da1cd5cdeb5316cf2
B1rtek <skrzeczek@onet.pl>
2022-08-20 17:53:02 +0200

    Added methods that will be used for the new Scheduler list

--- src/com/birtek/cashew/database/ScheduledMessagesDatabase.java
+++ src/com/birtek/cashew/database/ScheduledMessagesDatabase.java
@@ -82,12 +82,78 @@ public class ScheduledMessagesDatabase {
     }
 
     /**
+     * Gets a page of the {@link ScheduledMessage ScheduledMessages} from the specified server
+     *
+     * @param serverID   ID of the server from which the messages were requested
+     * @param pageNumber number of the page requested, starting from 1
+     * @return an ArrayList of {@link ScheduledMessage ScheduledMessages} representing the page, or null if an error
+     * occurred
+     */
+    public ArrayList<ScheduledMessage> getScheduledMessagesPage(String serverID, int pageNumber) {
+        try {
+            PreparedStatement preparedStatement = scheduledMessagesConnection.prepareStatement("SELECT _id, messagecontent, executiontime, destinationchannelid FROM (SELECT ROW_NUMBER() OVER(ORDER BY _id) index, _id, messagecontent, executiontime, destinationchannelid FROM scheduledmessages WHERE serverid = ?) AS subqr where index between (?-1)*10+1 and (?-1)*10+10");
+            preparedStatement.setString(1, serverID);
+            preparedStatement.setInt(2, pageNumber);
+            preparedStatement.setInt(3, pageNumber);
+            ResultSet results = preparedStatement.executeQuery();
+            return resultSetToArrayList(results);
+        } catch (SQLException e) {
+            LOGGER.warn(e + " thrown at ScheduledMessagesDatabase.getScheduledMessagesPage()");
+            return null;
+        }
+    }
+
+    /**
+     * Gets the number of pages that the whole scheduled messages list consists of, each page consists of 10 messages
+     *
+     * @param serverID ID of the server which page count will be obtained
+     * @return the number of pages of the list, or -1 if an error occurred
+     */
+    public int getScheduledMessagesPageCount(String serverID) {
+        try {
+            PreparedStatement preparedStatement = scheduledMessagesConnection.prepareStatement("SELECT COUNT(*) FROM scheduledmessages WHERE serverid = ?");
+            preparedStatement.setString(1, serverID);
+            ResultSet results = preparedStatement.executeQuery();
+            if (results.next()) {
+                return results.getInt(1) / 10 + (results.getInt(1) % 10 == 0 ? 0 : 1);
+            }
+            return 0;
+        } catch (SQLException e) {
+            LOGGER.warn(e + " thrown at ScheduledMessagesDatabase.getScheduledMessagesPageCount()");
+            return -1;
+        }
+    }
+
+    /**
+     * Gets the scheduled message with the provided index from the server's scheduled messages list
+     *
+     * @param serverID ID of the server from which list the message will be taken
+     * @param index    index of the message, starting from zero
+     * @return {@link ScheduledMessage ScheduledMessage} with the provided index, or null if it doesn't exist or if an
+     * error occurred
+     */
+    public ScheduledMessage getScheduledMessageByIndex(String serverID, int index) {
+        try {
+            PreparedStatement preparedStatement = scheduledMessagesConnection.prepareStatement("SELECT _id, messagecontent, executiontime, destinationchannelid FROM (SELECT ROW_NUMBER() OVER(ORDER BY _id) index, _id, messagecontent, executiontime, destinationchannelid FROM scheduledmessages WHERE serverid = ?) AS subqr where index = ?");
+            preparedStatement.setString(1, serverID);
+            preparedStatement.setInt(2, index + 1);
+            ResultSet results = preparedStatement.executeQuery();
+            if (results.next()) {
+                return new ScheduledMessage(results.getInt(1), results.getString(2), results.getString(3), results.getString(4));
+            }
+            return null;
+        } catch (SQLException e) {
+            LOGGER.warn(e + " thrown at ScheduledMessagesDatabase.getScheduledMessagesPage()");
+            return null;
+        }
+    }
+
+    /**
      * Adds a ScheduledMessage to the database by inserting it into the scheduledmessages table
      *
      * @param scheduledMessage ScheduledMessage to add
      * @param serverID         serverID of the server where the message was scheduled
      * @return the provided {@link ScheduledMessage ScheduledMessage} but with an ID assigned by the database, or null
-     *
      */
     public ScheduledMessage addScheduledMessage(ScheduledMessage scheduledMessage, String serverID) {
         try {
@@ -97,7 +163,7 @@ public class ScheduledMessagesDatabase {
             preparedStatement.setString(3, "86400");
             preparedStatement.setString(4, scheduledMessage.getDestinationChannelID());
             preparedStatement.setString(5, serverID);
-            if(preparedStatement.executeUpdate() != 1) return null;
+            if (preparedStatement.executeUpdate() != 1) return null;
             ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
             if (generatedKeys.next()) {
                 int scheduledMessageID = generatedKeys.getInt(1);
