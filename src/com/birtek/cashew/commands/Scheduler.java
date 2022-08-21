4ae18dc1565b54dfc102b36b8c8b00f78d41ddbe
B1rtek <skrzeczek@onet.pl>
2022-08-21 17:39:21 +0200

    Switching between pages in /scheduler list works

--- src/com/birtek/cashew/commands/Scheduler.java
+++ src/com/birtek/cashew/commands/Scheduler.java
@@ -8,9 +8,15 @@ import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.emoji.Emoji;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import net.dv8tion.jda.api.interactions.components.ActionRow;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
+import net.dv8tion.jda.api.interactions.components.selections.SelectMenu;
 import net.dv8tion.jda.api.utils.TimeFormat;
+import net.dv8tion.jda.internal.utils.tuple.Pair;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
@@ -22,11 +28,30 @@ import java.util.Objects;
  */
 public class Scheduler extends BaseCommand {
 
+    /**
+     * Gets a timestamp from Instant's toEpochMilli from the time string being the message send time
+     *
+     * @param timeString time on which the message is sent every day
+     * @return timestamp with the next execution date
+     */
     private long getHourTimestamp(String timeString) {
         return ScheduledMessagesManager.getInstantFromExecutionTime(timeString).toEpochMilli();
     }
 
-    private MessageEmbed generateScheduledMessagesEmbed(ArrayList<ScheduledMessage> messages, User user, Guild server) {
+    /**
+     * Generates an {@link MessageEmbed embed} with the provided {@link ScheduledMessage ScheduledMessages} list,
+     * showing their basic details and the beginning of the message
+     *
+     * @param messages   ArrayList of {@link ScheduledMessage ScheduledMessages} representing a whole page of them, a page
+     *                   consists of maximum of 10 messages
+     * @param server     {@link Guild server} from which the messages are listed
+     * @param pageNumber number of the page that will be generated
+     * @return a {@link MessageEmbed MessageEmbed} with the title "Scheduled messages on (server)", a list of messages
+     * with their beginning in the field name, and the destination channel and the delivery time in the description, the
+     * number of the current page in the footer as well as the number of total pages, and the server icon as the
+     * thumbnail
+     */
+    private MessageEmbed generateScheduledMessagesEmbed(ArrayList<ScheduledMessage> messages, Guild server, int pageNumber) {
         EmbedBuilder scheduledMessagesEmbed = new EmbedBuilder();
         scheduledMessagesEmbed.setTitle("Messages scheduled on " + server.getName());
         scheduledMessagesEmbed.setThumbnail(server.getIconUrl());
@@ -34,8 +59,47 @@ public class Scheduler extends BaseCommand {
             String messageContentShort = message.getMessageContent().length() > 64 ? message.getMessageContent().substring(0, 64) + "..." : message.getMessageContent();
             scheduledMessagesEmbed.addField(messageContentShort, "In <#" + message.getDestinationChannelID() + ">, " + TimeFormat.TIME_LONG.format(getHourTimestamp(message.getExecutionTime())), false);
         }
-        scheduledMessagesEmbed.setFooter("Select a message with the dropdown menu to see it's full content or delete it");
+        ScheduledMessagesDatabase database = ScheduledMessagesDatabase.getInstance();
+        int pageCount = database.getScheduledMessagesPageCount(server.getId());
+        scheduledMessagesEmbed.setFooter("Page " + pageNumber + " out of " + pageCount);
         return scheduledMessagesEmbed.build();
+    }
+
+    /**
+     * Creates a {@link Pair Pair} of ActionRows - one of them being a {@link SelectMenu SelectMenu} used to choose
+     * messages, and the other one with buttons to interact with them
+     *
+     * @param messages         ArrayList of {@link ScheduledMessage ScheduledMessages} representing a whole page of them, a page
+     *                         consists of maximum of 10 messages
+     * @param user             {@link User user} who will be able to interact with the buttons, the user who executed the
+     *                         /scheduler list command
+     * @param deleteAllConfirm if set to true, will generate with the "delete all confirmation" button instead of the
+     *                         "delete all" button
+     * @return a {@link Pair Pair} of {@link ActionRow ActionRows} - the first one with a {@link SelectMenu SelectMenu}
+     * with which the user selects the message to interact with, and the other one with a set of 5 buttons -
+     * "Show details", "Delete", "Delete all/confirm delete all", "<" and ">" (used to switch between pages)
+     */
+    private Pair<ActionRow, ActionRow> generateScheduledMessagesListActionRows(ArrayList<ScheduledMessage> messages, User user, boolean deleteAllConfirm) {
+        SelectMenu.Builder scheduledMessagesSelectMenu = SelectMenu.create(user.getId() + ":scheduler:list")
+                .setPlaceholder("Select a message")
+                .setRequiredRange(1, 1);
+        int index = 0;
+        for (ScheduledMessage message : messages) {
+            String messageContentShort = message.getMessageContent().length() > 64 ? message.getMessageContent().substring(0, 64) + "..." : message.getMessageContent();
+            scheduledMessagesSelectMenu.addOption(messageContentShort, String.valueOf(index));
+            index++;
+        }
+        ArrayList<Button> schedulerListButtons = new ArrayList<>();
+        schedulerListButtons.add(Button.success(user.getId() + ":scheduler:details", "Show details"));
+        schedulerListButtons.add(Button.danger(user.getId() + ":scheduler:delete", "Delete"));
+        if (deleteAllConfirm) {
+            schedulerListButtons.add(Button.danger(user.getId() + ":scheduler:deleteall2", "[!] Confirm delete all [!]"));
+        } else {
+            schedulerListButtons.add(Button.danger(user.getId() + ":scheduler:deleteall", "Delete all"));
+        }
+        schedulerListButtons.add(Button.primary(user.getId() + ":scheduler:page:0", Emoji.fromUnicode("◀️")));
+        schedulerListButtons.add(Button.primary(user.getId() + ":scheduler:page:1", Emoji.fromUnicode("▶️")));
+        return Pair.of(ActionRow.of(scheduledMessagesSelectMenu.build()), ActionRow.of(schedulerListButtons));
     }
 
     /**
@@ -113,11 +177,12 @@ public class Scheduler extends BaseCommand {
                     return;
                 }
                 if (messages.isEmpty()) {
-                    event.reply("There are no scheduled messages on this server").setEphemeral(true).queue();
+                    event.reply("This page does not exist").setEphemeral(true).queue();
                     return;
                 }
-                MessageEmbed scheduledMessagesEmbed = generateScheduledMessagesEmbed(messages, event.getUser(), event.getGuild());
-                event.replyEmbeds(scheduledMessagesEmbed).setEphemeral(true).queue();
+                MessageEmbed scheduledMessagesEmbed = generateScheduledMessagesEmbed(messages, event.getGuild(), page);
+                Pair<ActionRow, ActionRow> schedulerListActionRows = generateScheduledMessagesListActionRows(messages, event.getUser(), false);
+                event.replyEmbeds(scheduledMessagesEmbed).addActionRows(schedulerListActionRows.getLeft(), schedulerListActionRows.getRight()).setEphemeral(true).queue();
             } else if (event.getSubcommandName().equals("delete")) {
                 int id = event.getOption("id", 0, OptionMapping::getAsInt);
                 String definitely = event.getOption("all", "", OptionMapping::getAsString);
@@ -127,6 +192,54 @@ public class Scheduler extends BaseCommand {
                 }
                 String response = schedulerDeleteMessages(event.getGuild(), id);
                 event.reply(response).setEphemeral(true).queue();
+            }
+        }
+    }
+
+    private void showPage(ButtonInteractionEvent event, int page, boolean deleteAllConfirm) {
+        ScheduledMessagesDatabase database = ScheduledMessagesDatabase.getInstance();
+        int pageCount = database.getScheduledMessagesPageCount(Objects.requireNonNull(event.getGuild()).getId());
+        page = page < 1 ? 1 : (Math.min(page, pageCount));
+        ArrayList<ScheduledMessage> messages = database.getScheduledMessagesPage(Objects.requireNonNull(event.getGuild()).getId(), page);
+        if (messages == null) {
+            event.reply("Something went wrong while fetching the list of scheduled messages, try again later").setEphemeral(true).queue();
+            return;
+        }
+        if (messages.isEmpty()) {
+            event.reply("This page does not exist").setEphemeral(true).queue();
+            return;
+        }
+        MessageEmbed scheduledMessagesEmbed = generateScheduledMessagesEmbed(messages, event.getGuild(), page);
+        Pair<ActionRow, ActionRow> schedulerListActionRows = generateScheduledMessagesListActionRows(messages, event.getUser(), deleteAllConfirm);
+        event.editMessageEmbeds(scheduledMessagesEmbed).setActionRows(schedulerListActionRows.getLeft(), schedulerListActionRows.getRight()).queue();
+    }
+
+    @Override
+    public void onButtonInteraction(@NotNull ButtonInteractionEvent event) {
+        String[] buttonID = event.getComponentId().split(":");
+        if (buttonID.length < 3) return;
+        if (buttonID[1].equals("scheduler")) {
+            if (!event.getUser().getId().equals(buttonID[0])) {
+                event.reply("You can't interact with this embed").setEphemeral(true).queue();
+                return;
+            }
+            switch (buttonID[2]) {
+                case "details" -> {
+
+                }
+                case "delete" -> {
+
+                }
+                case "deleteall" -> {
+
+                }
+                case "deleteall2" -> {
+
+                }
+                case "page" -> {
+                    int page = getPageNumber(event.getMessage().getEmbeds().get(0)) + (buttonID[3].equals("1") ? 1 : -1);
+                    showPage(event, page, false);
+                }
             }
         }
     }
