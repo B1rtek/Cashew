baa75b94383b950891f8bd34e97f34d1df08ed2c
B1rtek <skrzeczek@onet.pl>
2022-09-08 23:08:15 +0200

    Created the MessageCache for all messages sent/edited in the last hour

---
+++ src/com/birtek/cashew/database/MessageCache.java
@@ -0,0 +1,57 @@
+package com.birtek.cashew.database;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MessageCache {
+
+    HashMap<String, HashMap<String, CachedMessage>> cachingMap;
+
+    public MessageCache() {
+        cachingMap = new HashMap<>();
+    }
+
+    /**
+     * Adds a new message to the cache and removes all messages older than 1 hour
+     *
+     * @param message  {@link CachedMessage message} to cache
+     * @param serverID ID of the server from which the message came
+     */
+    public void newMessage(CachedMessage message, String serverID) {
+        HashMap<String, CachedMessage> serverMessages = cachingMap.get(serverID);
+        if (serverMessages == null) {
+            serverMessages = new HashMap<>();
+        } else {
+            long currentTimestamp = Instant.now().toEpochMilli();
+            ArrayList<String> toRemove = new ArrayList<>();
+            for (String id : serverMessages.keySet()) {
+                if (currentTimestamp - serverMessages.get(id).lastChangeTimestamp() > 3600000) {
+                    toRemove.add(id);
+                }
+            }
+            for (String id : toRemove) {
+                serverMessages.remove(id);
+            }
+        }
+        serverMessages.put(message.messageID(), message);
+        cachingMap.put(serverID, serverMessages);
+    }
+
+    /**
+     * Gets the cached message with the provided ID
+     *
+     * @param messageID ID of the message to get
+     * @param serverID  ID of the server from which the message is requested
+     * @return the {@link CachedMessage CachedMessage} if it's still in the cache or null if it's not
+     */
+    public CachedMessage getMessage(String messageID, String serverID) {
+        HashMap<String, CachedMessage> serverMessages = cachingMap.get(serverID);
+        if (serverMessages == null) {
+            serverMessages = new HashMap<>();
+            return null;
+        } else {
+            return serverMessages.get(messageID);
+        }
+    }
+}
