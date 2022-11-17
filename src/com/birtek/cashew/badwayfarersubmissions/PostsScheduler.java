209e39211806d3f247e285038dc9034940efca59
b1rtek <skrzeczek@onet.pl>
2022-11-17 17:41:36 +0100

    Adjusted BadWayfarerBot's posts scheduler

--- src/com/birtek/cashew/badwayfarersubmissions/PostsScheduler.java
+++ src/com/birtek/cashew/badwayfarersubmissions/PostsScheduler.java
@@ -22,11 +22,11 @@ public class PostsScheduler {
         PostsDatabase database = PostsDatabase.getInstance();
         int postsCount = database.getVerifiedPostsCount();
         if (postsCount <= 0) postsCount = 1;
-        int range = 9 * 3600 / postsCount;
+        int range = (12-Math.max(postsCount, 2)) * 3600;
         Random random = new Random();
         int delay = random.nextInt(range);
         delay = verifyExecutionTime(delay);
-        System.out.println(delay);
+        //System.out.println(delay);
         scheduler.schedule(new SchedulerRunnable(), delay, TimeUnit.SECONDS);
     }
 
