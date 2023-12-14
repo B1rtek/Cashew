ee1d6b6ea7c6470861363305c956eec4ac75c4ed
b1rtek <skrzeczek@onet.pl>
2023-12-14 14:57:44 +0100

    Cashew status page test

---
+++ src/com/birtek/cashew/web/CashewStatusHttpHandler.java
@@ -0,0 +1,49 @@
+package com.birtek.cashew.web;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class CashewStatusHttpHandler implements HttpHandler {
+
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
+        String requestParamValue = null;
+        if ("GET".equals(httpExchange.getRequestMethod())) {
+            requestParamValue = handleGetRequest(httpExchange);
+        }
+        handleResponse(httpExchange, requestParamValue);
+    }
+
+    private String handleGetRequest(HttpExchange httpExchange) {
+        return httpExchange.
+                getRequestURI()
+                .toString()
+                .split("\\?")[1]
+                .split("=")[1];
+    }
+
+    private void handleResponse(HttpExchange httpExchange, String requestParamValue) throws IOException {
+        OutputStream outputStream = httpExchange.getResponseBody();
+        StringBuilder htmlBuilder = new StringBuilder();
+        htmlBuilder.append("<html>")
+                .append("<body>")
+                .append("<h1>")
+                .append("Hello ")
+                .append(requestParamValue)
+                .append("</h1>")
+                .append("</body>")
+                .append("</html>");
+        // encode HTML content
+//        String htmlResponse = StringEscapeUtils.escapeHtml4(htmlBuilder.toString());
+        String htmlResponse = htmlBuilder.toString();
+
+        // this line is a must
+        httpExchange.sendResponseHeaders(200, htmlResponse.length());
+        outputStream.write(htmlResponse.getBytes());
+        outputStream.flush();
+        outputStream.close();
+    }
+}
---
+++ src/com/birtek/cashew/web/CashewStatusServer.java
@@ -0,0 +1,31 @@
+package com.birtek.cashew.web;
+
+import com.sun.net.httpserver.HttpServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class CashewStatusServer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CashewStatusServer.class);
+
+    private static final ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool(2);
+
+    private HttpServer server;
+    public CashewStatusServer() {
+        try {
+            server = HttpServer.create(new InetSocketAddress("localhost",8008), 0);
+        } catch (IOException e) {
+            LOGGER.error("Failed to create CashewStatusServer");
+            return;
+        }
+
+        server.createContext("/test", new CashewStatusHttpHandler());
+        server.setExecutor(threadPoolExecutor);
+        server.start();
+    }
+}
